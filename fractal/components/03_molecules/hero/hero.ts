f7443139a4398f9bd8e0457b461837e3d9c96a57
Jerome Haas <hello@jeromehaas.dev>
2022-05-03 10:01:59 +0200

    Setup: hero component

---
+++ fractal/components/03_molecules/hero/hero.ts
@@ -0,0 +1,45 @@
+import { gsap } from 'gsap';
+import Swiper, { Autoplay, EffectFade, Navigation, Pagination, Zoom } from 'swiper';
+
+class Hero {
+	name: string;
+	elements: any;
+
+	constructor() {
+		this.name = 'hero';
+		this.elements = {
+			scroller: document.querySelector('.panel__scroller'),
+		};
+	};
+
+	init = () => {
+		if (document.querySelector(`js-${this.name}`)) return;
+		this.createSlider();
+		this.animateScroller();
+	};
+
+	createSlider = () => {
+		const swiper = new Swiper('.swiper', {
+			modules: [ Autoplay, EffectFade ],
+			speed: 1500,
+			freeMode: false,
+			loop: true,
+			spaceBetween: 30,
+			effect: "fade",
+			autoplay: {
+				delay: 15000,
+				disableOnInteraction: false,
+			}
+	});
+}
+
+	animateScroller = () => {
+		gsap.fromTo(this.elements.scroller, 
+			{ y: 0 },
+			{ y: 16, repeat: -1, yoyo: true, repeatDelay: 0.3, duration: 0.6, ease: 'power4.easeInOut' }
+		);
+	};
+
+};
+
+export default Hero;
--- fractal/public/js/main.ts
+++ fractal/public/js/main.ts
@@ -1,8 +1,12 @@
 import Cursor from '../../components/01_atoms/cursor/cursor';
 import ContactForm from '../../components/02_molecules/contact-form/contact-form';
+import Hero from '../../components/03_molecules/hero/hero';
 
 const cursor = new Cursor();
 cursor.init();
 
 const contactForm = new ContactForm();
 contactForm.init();
+
+const hero = new Hero();
+hero.init();
