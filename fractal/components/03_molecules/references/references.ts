5c6a52f1acda0ac4d1c04dc8d8712143183761ae
jeromehaas <hello@jeromehaas.ch>
2022-06-01 17:34:23 +0200

    Refactor: improve slider to make past items invisible

--- fractal/components/03_molecules/references/references.ts
+++ fractal/components/03_molecules/references/references.ts
@@ -1,12 +1,18 @@
+import { gsap } from 'gsap';
 import { Swiper, Navigation, Scrollbar } from 'swiper';
 
 class References {
 	name: string;
 	slider: any;
-
+	elements: any;
 
 	constructor() {
 		this.name = 'references';
+		this.elements = {
+			slider: {
+				items: document.querySelectorAll('.references__slider .slider__item')
+			}
+		},
 		this.slider = {
 			element: null,
 			configs: {
@@ -34,10 +40,21 @@ class References {
 
 	init = () => {
 		this.createCarousel();
+		this.hideSlides();
 	};
 
 	createCarousel = () => {
 		this.slider.element = new Swiper('.slider__container', this.slider.configs);
+	};
+
+	hideSlides = () => {
+		this.slider.element.on('slideChange', () => {
+			const index = this.slider.element.activeIndex;
+			const itemsToHide = [...this.elements.slider.items].slice(0, index);
+			const items = [...this.elements.slider.items];
+			gsap.to(items, { opacity: 1, duration: 1});
+			gsap.to(itemsToHide, { opacity: 0, duration: 1});
+		});
 	};
 
 };
